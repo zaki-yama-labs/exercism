807ad5d085fc4d390bd4bb67dfec9610ba6feb1c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-09-17 21:45:44 +0900

    [anagram] suggested solution

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,22 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
-    let mut result = HashSet::new();
-    let mut word_sorted: Vec<char> = word.to_lowercase().chars().collect();
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lower = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lower);
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate_lower = candidate.to_lowercase();
+            candidate_lower.len() == word_lower.len()
+                && candidate_lower != word_lower
+                && get_sorted(&candidate_lower) == word_sorted
+        })
+        .copied()
+        .collect()
+}
+
+fn get_sorted(word: &str) -> Vec<char> {
+    let mut word_sorted: Vec<char> = word.chars().collect();
     word_sorted.sort_unstable();
-
-    for &anagram in possible_anagrams {
-        // The solution cannot contain the input word
-        if word.to_lowercase() == anagram.to_lowercase() {
-            continue;
-        }
-        let mut anagram_sorted: Vec<char> = anagram.to_lowercase().chars().collect();
-        anagram_sorted.sort_unstable();
-
-        if word_sorted == anagram_sorted {
-            result.insert(anagram);
-        }
-    }
-
-    result
+    word_sorted
 }
