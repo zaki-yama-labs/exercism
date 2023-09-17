4c94a255ec5244cbfb4347ab058d54591bfaa611
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-09-17 21:25:03 +0900

    [anagram] submit

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
+    let mut result = HashSet::new();
+    let mut word_sorted: Vec<char> = word.to_lowercase().chars().collect();
+    word_sorted.sort_unstable();
+
+    for &anagram in possible_anagrams {
+        // The solution cannot contain the input word
+        if word.to_lowercase() == anagram.to_lowercase() {
+            continue;
+        }
+        let mut anagram_sorted: Vec<char> = anagram.to_lowercase().chars().collect();
+        anagram_sorted.sort_unstable();
+
+        if word_sorted == anagram_sorted {
+            result.insert(anagram);
+        }
+    }
+
+    result
 }
