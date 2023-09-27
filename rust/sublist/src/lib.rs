a3c124385c549061c6365bd9768daa9af85bf8b7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-09-27 22:42:48 +0900

    [sublist] submit

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,5 +7,22 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+    let (larger_list, smaller_list) = if _first_list.len() > _second_list.len() {
+        (_first_list, _second_list)
+    } else {
+        (_second_list, _first_list)
+    };
+    for i in 0..=larger_list.len() - smaller_list.len() {
+        if smaller_list == &larger_list[i..i + smaller_list.len()] {
+            if larger_list == _first_list {
+                return Comparison::Superlist;
+            } else {
+                return Comparison::Sublist;
+            }
+        }
+    }
+    return Comparison::Unequal;
 }
