7e19a2f40a5bbd5389dd22be4f9fe0ca05460c3e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-10-11 08:56:33 +0900

    copilotのままに

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -6,9 +6,26 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
         .map(|(i, row)| {
             row.chars()
                 .enumerate()
-                .map(|(j, c)| {
-                    print!("{}", c);
-                    return c;
+                .map(|(j, c)| match c {
+                    '*' => '*',
+                    _ => {
+                        let mut count = 0;
+                        for x in i.saturating_sub(1)..=i + 1 {
+                            for y in j.saturating_sub(1)..=j + 1 {
+                                if x < minefield.len()
+                                    && y < row.len()
+                                    && minefield[x].chars().nth(y).unwrap() == '*'
+                                {
+                                    count += 1;
+                                }
+                            }
+                        }
+                        if count > 0 {
+                            std::char::from_digit(count, 10).unwrap()
+                        } else {
+                            ' '
+                        }
+                    }
                 })
                 .collect()
         })
